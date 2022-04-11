7df5898a09ddcd1556a22755c8698a07b4fa7d8f
Alejandro_Osendi <alejandro.osendi@doubledot.es>
2022-04-11 02:22:32 +0200

    Change likes to only an icon

---
+++ js/src/forum/addLikesList.js
@@ -0,0 +1,25 @@
+import { extend } from 'flarum/common/extend';
+import app from 'flarum/forum/app';
+import CommentPost from 'flarum/forum/components/CommentPost';
+import icon from 'flarum/common/helpers/icon';
+
+import PostLikesModal from './components/PostLikesModal';
+
+export default function () {
+  extend(CommentPost.prototype, 'footerItems', function (items) {
+    const post = this.attrs.post;
+    const likes = post.likes();
+
+    if (likes && likes.length) {
+      items.add(
+        'liked',
+        <a href="#" onclick={(e) => { e.preventDefault(); app.modal.show(PostLikesModal,{ post });}}>
+          <div className="Post-likedBy">
+            {icon('far fa-thumbs-up')}
+            {likes.length}
+          </div>
+        </a>
+      );
+    }
+  });
+}
